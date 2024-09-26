bceccb280011e66a44df87a918ca2e509b0febb1
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-09-26 06:53:23 +0530

    first

---
+++ src/main/java/com/learn/spring_hateoas_demo/SpringHateoasDemoApplication.java
@@ -0,0 +1,13 @@
+package com.learn.spring_hateoas_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHateoasDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHateoasDemoApplication.class, args);
+	}
+
+}
